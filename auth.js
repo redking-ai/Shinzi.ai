625b2297a67099a170dc31a43396629fc97a1a65
Red king <sboficial226@gmail.com>
2026-04-28 22:33:39 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -9,29 +9,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCfRMspgRtP-d3Jnha8DK7q4X8Buhj6qHA",
-    authDomain: "shinzi-ai.firebaseapp.com",
-    projectId: "shinzi-ai",
-    storageBucket: "shinzi-ai.firebasestorage.app",
-    messagingSenderId: "650971065920",
-    appId: "1:650971065920:web:cce30c99c3a9572b95f9a5",
-    measurementId: "G-P28XFTCSCX"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>",
+  apiKey: "AIzaSyCfRMspgRtP-d3Jnha8DK7q4X8Buhj6qHA",
   authDomain: "shinzi-ai.firebaseapp.com",
   projectId: "shinzi-ai",
   storageBucket: "shinzi-ai.firebasestorage.app",
