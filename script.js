d3fb948a9e3cc1838b7b6dd29ad1ec06fe395c6d
Red king <sboficial226@gmail.com>
2026-04-29 16:35:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const GEMINI_API_KEY = "AIzaSyASEmxQcddG-E97vXdb0W9clwqM16wt-BQ";
-const GEMINI_MODEL = "gemini-2.0-flash";
+const GEMINI_MODEL = "gemini-1.5-flash";
 const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}:generateContent?key=${GEMINI_API_KEY}`;
 
 const MSG_LIMIT = 30;
